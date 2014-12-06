92c5b30c15292a6802a5791b0a628095bedd05e4
Satimidus <satimidus@gmail.com>
2014-12-06 17:56:52 -0500

    redirected node.js server to index.html

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ response.end();
 return;
 }
 
-if (fs.statSync(filename).isDirectory()) filename += '/gallery.html';
+if (fs.statSync(filename).isDirectory()) filename += '/index.html';
 
 fs.readFile(filename, "binary", function(err, file) {
 if(err) {        
