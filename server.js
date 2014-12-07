f75816406e1dab3cd5e728939cb530dc6d4b43e5
Satimidus <satimidus@gmail.com>
2014-12-07 11:49:43 -0500

    resized everything because this isn't for ants.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require("http"),
 url = require("url"),
 path = require("path"),
-fs = require("fs")
+fs = require("fs"),
 port = process.argv[2] || 8888;
 
 http.createServer(function(request, response) {
